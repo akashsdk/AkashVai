4685ab9c210c75d0871cf65823ff734a113ff70b
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-09 01:37:12 +0600

    Update App.js

--- App.js
+++ App.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import { ScrollView, View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import  Button  from './component/Button';
+
+export default function App() {
+  const [a, setA] 
+}
 const App = () => {
   
   return (
