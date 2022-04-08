6b7d63373664f2f1556774eaebac97362ffedeea
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-09 01:43:40 +0600

    Update App.js

--- App.js
+++ App.js
@@ -2,35 +2,11 @@ import React from 'react';
 import { ScrollView, View, Text, TouchableOpacity, Dimensions } from 'react-native';
 import  Button  from './component/Button';
 
-
-const App = () => {
-  
+export default function App() {
+  const [a, setA] = userSelect ('ok')
   return (
     <View>
-    <Button/>
-      <View style={{
-        height: 70,
-        marginTop: 41,
-        display: 'flex',
-        flexDirection: 'row'
-
-      }}>
-        <View style={{
-          backgroundColor: 'red',
-          height: 30,
-          width: 30,
-          borderRadius: 15,
-          margin: 20,
-        }}></View>
-        <Text style={{
-          margin: 20,
-          fontSize: 30,
-        }}>Sathi Shikdar fnot</Text>
-      </View>
-      <ScrollView>
-
-      </ScrollView>
+      <Button/>
     </View>
   )
 }
-export default App;
