79983f244d5b5888278e38dcdb9139a992f5d23e
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-09 01:41:31 +0600

    Update App.js

--- App.js
+++ App.js
@@ -8,36 +8,5 @@ export default function App() {
     <View>
       <Button></Button>
     </View>
-  )
-}
-const App = () => {
-  
-  return (
-    <View>
-    <Button/>
-      <View style={{
-        height: 70,
-        marginTop: 41,
-        display: 'flex',
-        flexDirection: 'row'
-
-      }}>
-        <View style={{
-          backgroundColor: 'red',
-          height: 30,
-          width: 30,
-          borderRadius: 15,
-          margin: 20,
-        }}></View>
-        <Text style={{
-          margin: 20,
-          fontSize: 30,
-        }}>Sathi Shikdar fnot</Text>
-      </View>
-      <ScrollView>
-
-      </ScrollView>
-    </View>
-  )
-}
-export default App;
+  );
+};
