0e6410db122da93c92b5db353fb6a46b154c10ae
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-10 17:02:30 +0600

    Update App.js

--- App.js
+++ App.js
@@ -10,7 +10,6 @@ export default function App() {
       <Button a={a} setA={setA}/>
       <Button a={a} color='blue' setA={setA}/>
       <Button a={a} setA={setA}/>
-      <Button a={a} color='red' setA={setA}/>
     </View>
   );
 }
